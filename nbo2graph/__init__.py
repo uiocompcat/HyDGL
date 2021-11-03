07435383f6b8f850b0c50d27973e168896dec042
Hannes Kneiding <hanneskn@uio.no>
2021-11-03 11:25:22 +0100

    packaging setup file

---
+++ nbo2graph/__init__.py
@@ -0,0 +1,5 @@
+from .data_parser import DataParser
+from .graph_generator import GraphGenerator
+from .graph_generator_settings import GraphGeneratorSettings
+
+__version__ = 'dev'
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from nbo2graph import __version__
+
+setup(
+    name='nbo2graph',
+    version=__version__,
+
+    url='https://github.com/hkneiding/nbo2graph',
+    author='Hannes Kneiding',
+    author_email='hannes.kneiding@outlook.com',
+
+    py_modules=['nbo2graph'],
+)
