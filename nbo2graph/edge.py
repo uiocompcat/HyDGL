11c7fdddafb9a460a8df791063012a39753a9735
Hannes Kneiding <hanneskn@uio.no>
2021-11-24 17:11:15 +0100

    removed comparison interface

--- nbo2graph/edge.py
+++ nbo2graph/edge.py
@@ -16,13 +16,6 @@ class Edge:
         self._features = features
         self._is_directed = is_directed
 
-    def __eq__(self, other):
-
-        """Equality interface that allows comparsion between objects for unit testing"""
-        return self._node_indices == other.node_indices and \
-            self._features == other.features and \
-            self._is_directed == other.is_directed
-
     @property
     def node_indices(self):
         """Getter for node_indices"""
