f15903914da4d85596e135565056cd7f1803bc90
Hannes Kneiding <hanneskn@uio.no>
2021-11-24 17:10:16 +0100

    refactored tests and added additional testing utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,7 @@
 import unittest
 
+from nbo2graph.edge import Edge
+
 
 class Utils():
 
@@ -12,8 +14,20 @@ class Utils():
 
         """Deep asserts almost equality between two objects."""
 
+        # assert equal data types
+        Utils.tc.assertEqual(type(a), type(b))
+
+        # check data type and call appropriate comparison function
+        comparison_function = Utils.get_comparison_function(type(a))
+        comparison_function(a, b, places=places)
+
+    @staticmethod
+    def base_assert_are_almost_equal(a, b, places=5):
+
+        """Deep asserts almost equality between two items (can be multidim lists or objects)."""
+
         # check if not list
-        if type(a) != list:
+        if type(a) in [str, int, float, bool]:
             Utils.tc.assertAlmostEqual(a, b, places=places)
             # return a == b
         else:
@@ -24,3 +38,20 @@ class Utils():
             # recursively call
             for a_, b_ in zip(a, b):
                 Utils.assert_are_almost_equal(a_, b_, places=places)
+
+    @staticmethod
+    def get_comparison_function(data_type):
+
+        if data_type == Edge:
+            return Utils.edge_assert_are_almost_equal
+        else:
+            return Utils.base_assert_are_almost_equal
+
+    @staticmethod
+    def edge_assert_are_almost_equal(a: Edge, b: Edge, places=5):
+
+        """Equality operator for edge objects."""
+
+        Utils.assert_are_almost_equal(a.features, b.features, places=places)
+        Utils.assert_are_almost_equal(a.node_indices, b.node_indices, places=places)
+        Utils.assert_are_almost_equal(a.is_directed, b.is_directed, places=places)
