4f1b3c5ea3300927c05ee25139e96ad3ba48e5a7
Hannes Kneiding <hanneskn@uio.no>
2021-12-20 10:37:09 +0100

    removed support for reading graph building settings from file

--- tests/test_graph_generator_settings.py
+++ tests/test_graph_generator_settings.py
@@ -1,10 +1,6 @@
 import unittest
 from parameterized import parameterized
 
-from nbo2graph.enums.node_feature import NodeFeature
-from nbo2graph.enums.edge_feature import EdgeFeature
-from nbo2graph.enums.graph_feature import GraphFeature
-from nbo2graph.enums.qm_target import QmTarget
 from nbo2graph.enums.hydrogen_mode import HydrogenMode
 from nbo2graph.enums.bond_determination_mode import BondDeterminationMode
 from nbo2graph.graph_generator_settings import GraphGeneratorSettings
@@ -26,17 +22,17 @@ class TestGraphGeneratorSettings(unittest.TestCase):
                                    hydrogen_count_threshold=0.5)
         ],
 
-        [
-            GraphGeneratorSettings.from_file('./tests/files/test.config'),
-            GraphGeneratorSettings(node_features=[NodeFeature.ATOMIC_NUMBERS, NodeFeature.LMO_BOND_ORDER_TOTAL, NodeFeature.NATURAL_ELECTRON_CONFIGURATION_F, NodeFeature.LONE_PAIR_ENERGY_MIN_MAX_DIFFERENCE, NodeFeature.LONE_PAIR_MAX, NodeFeature.LONE_PAIRS_S, NodeFeature.LONE_VACANCIES_S],
-                                   edge_features=[EdgeFeature.NLMO_BOND_ORDER, EdgeFeature.BOND_ORBITAL_AVERAGE, EdgeFeature.BOND_ORBITAL_DATA_F, EdgeFeature.ANTIBOND_ENERGY_MIN_MAX_DIFFERENCE, EdgeFeature.ANTIBOND_ORBITAL_MIN, EdgeFeature.ANTIBOND_ORBITAL_AVERAGE],
-                                   graph_features=[GraphFeature.CHARGE],
-                                   targets=[QmTarget.POLARISABILITY, QmTarget.LOWEST_VIBRATIONAL_FREQUENCY],
-                                   bond_determination_mode=BondDeterminationMode.NLMO,
-                                   bond_threshold=0.123,
-                                   hydrogen_mode=HydrogenMode.OMIT,
-                                   hydrogen_count_threshold=0.456)
-        ],
+        # [
+        #     GraphGeneratorSettings.from_file('./tests/files/test.config'),
+        #     GraphGeneratorSettings(node_features=[NodeFeature.ATOMIC_NUMBERS, NodeFeature.LMO_BOND_ORDER_TOTAL, NodeFeature.NATURAL_ELECTRON_CONFIGURATION_F, NodeFeature.LONE_PAIR_ENERGY_MIN_MAX_DIFFERENCE, NodeFeature.LONE_PAIR_MAX, NodeFeature.LONE_PAIRS_S, NodeFeature.LONE_VACANCIES_S],
+        #                            edge_features=[EdgeFeature.NLMO_BOND_ORDER, EdgeFeature.BOND_ORBITAL_AVERAGE, EdgeFeature.BOND_ORBITAL_DATA_F, EdgeFeature.ANTIBOND_ENERGY_MIN_MAX_DIFFERENCE, EdgeFeature.ANTIBOND_ORBITAL_MIN, EdgeFeature.ANTIBOND_ORBITAL_AVERAGE],
+        #                            graph_features=[GraphFeature.CHARGE],
+        #                            targets=[QmTarget.POLARISABILITY, QmTarget.LOWEST_VIBRATIONAL_FREQUENCY],
+        #                            bond_determination_mode=BondDeterminationMode.NLMO,
+        #                            bond_threshold=0.123,
+        #                            hydrogen_mode=HydrogenMode.OMIT,
+        #                            hydrogen_count_threshold=0.456)
+        # ],
 
     ])
     def test_graph_generator_settings_from_file(self, ggs, expected):
