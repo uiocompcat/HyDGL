96d35710370d5f2995748a7c54f48b87145e9810
Hannes Kneiding <hanneskn@uio.no>
2022-02-10 09:35:52 +0100

    added function to get one-hot encoding

--- nbo2graph/graph_element.py
+++ nbo2graph/graph_element.py
@@ -1,3 +1,6 @@
+from nbo2graph.tools import Tools
+
+
 class GraphElement:
 
     """Abstract class for elements of a graph."""
@@ -26,3 +29,13 @@ class GraphElement:
     def feature_list(self):
         """Getter for a list of features."""
         return [self._features[key] for key in self._features.keys()]
+
+    def get_one_hot_encoded_feature_list(self, class_feature_dict: dict) -> list[float]:
+
+        """Returns a feature list with class-type/string in one-hot encoding.
+
+        Returns:
+            list[float]: The feature list with one-hot encodings.
+        """
+
+        return Tools.get_one_hot_encoded_feature_list(self.features, class_feature_dict)
