627ca23a47fdb5b5f4331d1efd412c1d5197f2e5
Hannes Kneiding <k.1996@live.de>
2022-02-22 15:18:39 +0100

    fixed a bug in packaging

--- nbo2graph/__init__.py
+++ nbo2graph/__init__.py
@@ -1,4 +1,1 @@
-from .graph_generator import GraphGenerator
-from .graph_generator_settings import GraphGeneratorSettings
-
-__version__ = 'dev'
+__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-
 from nbo2graph import __version__
 
 setup(
@@ -9,6 +8,5 @@ setup(
     url='https://github.com/hkneiding/nbo2graph',
     author='Hannes Kneiding',
     author_email='hannes.kneiding@outlook.com',
-
-    py_modules=['nbo2graph'],
+    packages=['nbo2graph']
 )
