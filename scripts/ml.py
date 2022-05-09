7ecbe7650b8e60a9396e12c5553c60658176ee28
Hannes Kneiding <hanneskn@uio.no>
2022-05-09 11:13:04 +0200

    added runs for natq3

--- scripts/ml.py
+++ scripts/ml.py
@@ -6,7 +6,7 @@ import wandb
 from tmQMg import tmQMg
 from nbo2graph.enums.qm_target import QmTarget
 from nbo2graph.graph_generator_settings import GraphGeneratorSettings
-from nets import GilmerNetGraphLevelFeaturesDropout, GilmerNetGraphLevelFeaturesEdgeDropout, GilmerNetGraphLevelFeaturesLayerNorm
+from nets import GilmerNetGraphLevelFeatures, GilmerNetGraphLevelFeaturesDropout, GilmerNetGraphLevelFeaturesEdgeDropout, GilmerNetGraphLevelFeaturesLayerNorm
 from trainer import Trainer
 from tools import get_feature_matrix_dict, get_feature_means_from_feature_matrix_dict, get_feature_stds_from_feature_matrix_dict, set_global_seed, standard_scale_dataset
 from plot import plot_metal_center_group_histogram, plot_correlation, plot_error_by_metal_center_group, wandb_plot_error_by_metal_center_group
@@ -115,32 +115,31 @@ def run_ml(hyper_param: dict, wandb_project_name: str = 'tmQMg-natQgraph2', wand
 
 def run():
 
-    with open('/home/hkneiding/Downloads/outliers_polarizability.pickle', 'rb') as fh:
+    with open('/home/hkneiding/Downloads/outliers_full.pickle', 'rb') as fh:
         outliers = pickle.load(fh)
 
     hyper_param = {
         'name': 'layer norm test',
         'data': {
             'dataset': tmQMg,
-            'root_dir': '/home/hkneiding/Desktop/pyg-dataset-test-dir/run-natq2-extended/',
+            'root_dir': '/home/hkneiding/Desktop/pyg-dataset-test-dir/run-natq3-pol/',
             'raw_dir': '/home/hkneiding/Documents/UiO/Data/tmQMg/extracted/',
             'val_set_size': 0.1,
             'test_set_size': 0.1,
-            'graph_representation': GraphGeneratorSettings.natQ2extended,
+            'graph_representation': GraphGeneratorSettings.natQ3,
             'targets': [QmTarget.POLARISABILITY],
             'outliers': outliers
         },
         'model': {
             'name': 'GilmerNet',
-            'method': GilmerNetGraphLevelFeaturesDropout,
+            'method': GilmerNetGraphLevelFeatures,
             'parameters': {
                 'n_node_features': 21,
-                'n_edge_features': 18,
+                'n_edge_features': 23,
                 'n_graph_features': 4,
                 'dim': 128,
                 'set2set_steps': 4,
-                'n_atom_jumps': 4,
-                'dropout_rate': 0.1
+                'n_atom_jumps': 4
             }
         },
         'optimizer': {
@@ -174,27 +173,85 @@ def run():
 
 def run1():
 
-    with open('/home/hkneiding/Downloads/outliers_polarizability.pickle', 'rb') as fh:
+    with open('/home/hkneiding/Downloads/outliers_full.pickle', 'rb') as fh:
         outliers = pickle.load(fh)
 
     hyper_param = {
         'name': 'layer norm test',
         'data': {
             'dataset': tmQMg,
-            'root_dir': '/home/hkneiding/Desktop/pyg-dataset-test-dir/run-natq2-extended/',
+            'root_dir': '/home/hkneiding/Desktop/pyg-dataset-test-dir/run-natq3-hlgap/',
             'raw_dir': '/home/hkneiding/Documents/UiO/Data/tmQMg/extracted/',
             'val_set_size': 0.1,
             'test_set_size': 0.1,
-            'graph_representation': GraphGeneratorSettings.natQ2extended,
-            'targets': [QmTarget.POLARISABILITY],
+            'graph_representation': GraphGeneratorSettings.natQ3,
+            'targets': [QmTarget.TZVP_HOMO_LUMO_GAP],
             'outliers': outliers
         },
         'model': {
             'name': 'GilmerNet',
-            'method': GilmerNetGraphLevelFeaturesLayerNorm,
+            'method': GilmerNetGraphLevelFeatures,
             'parameters': {
                 'n_node_features': 21,
-                'n_edge_features': 18,
+                'n_edge_features': 23,
+                'n_graph_features': 4,
+                'dim': 128,
+                'set2set_steps': 4,
+                'n_atom_jumps': 4
+            }
+        },
+        'optimizer': {
+            'name': 'Adam',
+            'method': torch.optim.Adam,
+            'parameters': {
+                'lr': 0.001
+            }
+        },
+        'scheduler': {
+            'name': 'ReduceLrOnPlateau',
+            'method': torch.optim.lr_scheduler.ReduceLROnPlateau,
+            'parameters': {
+                'mode': 'min',
+                'factor': 0.7,
+                'patience': 5,
+                'min_lr': 0.00001
+            }
+        },
+        'scaling': {
+            'type': 'standard',
+            'features_to_scale': ['x', 'edge_attr', 'graph_attr', 'y']
+        },
+        'batch_size': 32,
+        'n_epochs': 300,
+        'seed': 2022
+    }
+
+    run_ml(hyper_param)
+
+
+def run2():
+
+    with open('/home/hkneiding/Downloads/outliers_full.pickle', 'rb') as fh:
+        outliers = pickle.load(fh)
+
+    hyper_param = {
+        'name': 'layer norm test',
+        'data': {
+            'dataset': tmQMg,
+            'root_dir': '/home/hkneiding/Desktop/pyg-dataset-test-dir/run-natq3-dipolm/',
+            'raw_dir': '/home/hkneiding/Documents/UiO/Data/tmQMg/extracted/',
+            'val_set_size': 0.1,
+            'test_set_size': 0.1,
+            'graph_representation': GraphGeneratorSettings.natQ3,
+            'targets': [QmTarget.TZVP_DIPOLE_MOMENT],
+            'outliers': outliers
+        },
+        'model': {
+            'name': 'GilmerNet',
+            'method': GilmerNetGraphLevelFeatures,
+            'parameters': {
+                'n_node_features': 21,
+                'n_edge_features': 23,
                 'n_graph_features': 4,
                 'dim': 128,
                 'set2set_steps': 4,
@@ -237,4 +294,5 @@ if __name__ == "__main__":
     # run = api.run("hkneiding/tmQMg-natQgraph2/17j02lpm")
 
     run()
-    # run1()
+    run1()
+    run2()
