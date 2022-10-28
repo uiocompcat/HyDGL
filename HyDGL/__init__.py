94e8658991b80ee9140c733b72a086c95a47534f
Hannes Kneiding <hanneskn@uio.no>
2022-10-28 10:35:50 -0400

    updated exported modules

--- HyDGL/__init__.py
+++ HyDGL/__init__.py
@@ -2,5 +2,6 @@ from .qm_data import QmData
 from .graph import Graph
 from .graph_generator import GraphGenerator
 from .graph_generator_settings import GraphGeneratorSettings
+from .element_look_up_table import ElementLookUpTable
 
 __version__ = '0.1'
