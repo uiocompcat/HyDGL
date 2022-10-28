b7133ffeb1bef532c795ed98b95f0c71fafcad6c
Hannes Kneiding <hanneskn@uio.no>
2022-10-28 10:38:56 -0400

    updated exported modules

--- HyDGL/__init__.py
+++ HyDGL/__init__.py
@@ -3,5 +3,6 @@ from .graph import Graph
 from .graph_generator import GraphGenerator
 from .graph_generator_settings import GraphGeneratorSettings
 from .element_look_up_table import ElementLookUpTable
+from .file_handler import FileHandler
 
 __version__ = '0.1'
