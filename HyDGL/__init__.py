fb4d34143e7ef5d5971098a43afd81ad7ac35519
Hannes Kneiding <33654921+hkneiding@users.noreply.github.com>
2026-02-23 09:49:20 +0100

    added lazy exports

--- HyDGL/__init__.py
+++ HyDGL/__init__.py
@@ -7,4 +7,30 @@ __all__ = [
     "FileHandler",
 ]
 
+def QmData(*args, **kwargs):
+    from .qm_data import QmData as _QmData
+    return _QmData(*args, **kwargs)
+
+def Graph(*args, **kwargs):
+    from .graph import Graph as _Graph
+    return _Graph(*args, **kwargs)
+
+def GraphGenerator(*args, **kwargs):
+    from .graph_generator import GraphGenerator as _GraphGenerator
+    return _GraphGenerator(*args, **kwargs)
+
+def GraphGeneratorSettings(*args, **kwargs):
+    from .graph_generator_settings import (
+        GraphGeneratorSettings as _GraphGeneratorSettings,
+    )
+    return _GraphGeneratorSettings(*args, **kwargs)
+
+def ElementLookUpTable(*args, **kwargs):
+    from .element_look_up_table import ElementLookUpTable as _ElementLookUpTable
+    return _ElementLookUpTable(*args, **kwargs)
+
+def FileHandler(*args, **kwargs):
+    from .file_handler import FileHandler as _FileHandler
+    return _FileHandler(*args, **kwargs)
+
 __version__ = '0.1'
