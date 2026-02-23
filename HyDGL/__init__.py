4cb4e659e930dcd3519f1bb533b77ce8d73c52a1
Hannes Kneiding <33654921+hkneiding@users.noreply.github.com>
2026-02-23 09:43:13 +0100

    fixed top level imports

--- HyDGL/__init__.py
+++ HyDGL/__init__.py
@@ -1,8 +1,10 @@
-from .qm_data import QmData
-from .graph import Graph
-from .graph_generator import GraphGenerator
-from .graph_generator_settings import GraphGeneratorSettings
-from .element_look_up_table import ElementLookUpTable
-from .file_handler import FileHandler
+__all__ = [
+    "QmData",
+    "Graph",
+    "GraphGenerator",
+    "GraphGeneratorSettings",
+    "ElementLookUpTable",
+    "FileHandler",
+]
 
 __version__ = '0.1'
